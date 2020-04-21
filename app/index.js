d08a0d11ab3bbe08dcae0b1915f6077948dee6c9
Jun Largo <rklargsjr@gmail.com>
2020-04-21 18:21:21 +0800

    add className container inside a div

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/popular'
 
 
 class App extends React.Component {
     render() {
         return(
-            <h2>
-                Hello World - 2
-            </h2>
+            <div className ='container'>
+                <Popular/>
+            </div>
         )
     }
 }
