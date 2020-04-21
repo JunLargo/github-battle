ff1dbee2a0f53a01089bbec738c87cba41928cbf
Jun Largo <rklargsjr@gmail.com>
2020-04-21 18:20:09 +0800

    create a navbar state

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,13 +1,35 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+        constructor(props) {
+            super(props)
+
+            this.state = {
+                selectedLanguage: 'All'
+            }
+
+            this.updateLanguage = this.updateLanguage.bind(this)
+        }
+
+        updateLanguage(selectedLanguage) {
+            this.setState({
+                selectedLanguage
+            })
+        }
+
     render() {
+        
         const languages = ['All', 'JavaScript', 'Java', 'CSS', 'Python']
         return(
-            <ul className ='flex-center'>
+            <ul className = 'flex-center'>
                 {languages.map((language) => (
-                    <li key = {language}>
-                        <button className='btn-clear nav-link'>
+                    <li key={language}>
+                        <button 
+                        className ='btn-clear nav-link'
+                        style = {language === this.state.selectedLanguage 
+                        ? {color: 'purple'}
+                        : null}
+                        onClick = {() => this.updateLanguage(language)}>
                             {language}
                         </button>
                     </li>
